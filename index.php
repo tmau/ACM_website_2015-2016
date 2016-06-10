f4eaef040823e348c7b4212185e45b4ba1e63326
tmau <tmau@scu.edu>
2016-06-09 22:00:46 -0700

    Website for SCUACM
    This was the website I created as webmaster for Santa Clara’s ACM
    chapter.

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+	<title>Santa Clara ACM</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>    
+    <!-- particles.js container -->
+	<div id="particles-js"></div>
+	<!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
+	<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<script src="js/particles.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="http://acm.engr.scu.edu/h4h/scrolling.js"></script>
+  </head>
+  <body>
+
+	<header class="navbar navbar-inverse navbar-fixed-top navbar-custom" role="navigation">
+		<div class="container-fluid" id="acm-navbar">
+		   <div class="navbar-header">  
+		    <p id="name">ACM</p>
+
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>                           
+		      </button> 
+		    </div>
+			 <nav class="collapse navbar-collapse" id="myNavbar">
+			    <ul class="nav navbar-nav navbar-right">  
+					<li><a href="/">Home</a></li>
+					<li><a href="/board.html">Board</a></li>
+					<li><a href="/events.html">Events</a></li>
+					<li><a href="/join.html">Join</a></li>
+					<!-- <li><a href="http://acm.engr.scu.edu/pay.html">Pay</a></li> -->
+			    </ul>
+		    </nav>
+		</div>
+	</nav>
+	<div id = "break"></div>
+	<div id="text">
+		<h1>ACM</h1>
+		<hr id="dank-line">
+		<p id ="def">Association for Computing Machinery</p>
+		<p id = "def2">Join the best and the brightest in tech at Santa Clara University</p>
+		<p id = "info">Meetings every Tuesday 5:30 PM @ Bannan Engineering Lounge (across ENGR 107) </p>
+		<!-- <p id = "email">Email santaclara.acm@gmail.com to join the mailing list<p> -->
+	</div>
+  </body>
+</html>
